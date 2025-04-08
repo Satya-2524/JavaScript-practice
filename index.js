0326a5e5ecd0449fa073839f0e576e681181545b
Satya-2524 <satyabhavani615@gmail.com>
2025-04-08 10:53:49 +0530

    js modules

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("This is a first class")
+function hello(){
+    a = 10;
+    b = 20;
+    c = a + b;
+    return c;
+}
+console.log(hello())
+
+// subtraction
+function sub(){
+    a = 30;
+    b = 20;
+    c = a - b;
+    return c;
+}
+
+console.log(sub())
+
+// multiplication
+function multi(){
+    a = 15;
+    b = 5;
+    c = a * b;
+    return c;
+}
+console.log(multi())
